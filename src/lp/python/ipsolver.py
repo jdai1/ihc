f33c51b2dd1ad07f8518f8770ba2b4367fb05dd0
jdai1 <julian.dai@gmail.com>
2025-04-12 16:19:54 -0400

    .

--- src/lp/python/ipsolver.py
+++ src/lp/python/ipsolver.py
@@ -1,5 +1,6 @@
 from typing import List
 import math
+
 from lpsolver import LPSolver
 import heapq
 import numpy as np
@@ -92,7 +93,8 @@ class DFSIPSolver:
 class BFSIPSolver:
     def __init__(self, filename: str):
         self.num_tests, self.num_diseases, self.cost, self.A = self.data_parse(filename)  # Optional if needed later
-        self.heap: List[tuple[float, list[int]]] = []
+        # ((cost, assignment, diff_diseases) pairs for BeFS)
+        self.heap: List[tuple[float, list[int], np.ndarray]] = []
         self.lp_solver = LPSolver(filename=filename)
         self.incumbent_cost: float = math.inf
         self.incumbent_assignment: List[float] = [0.0] * self.num_tests
@@ -102,13 +104,16 @@ class BFSIPSolver:
         for i in range(self.num_diseases):
             for j in range(i + 1, self.num_diseases):
                 table.append((self.A[:, i] - self.A[:, j]) ** 2)
-        table = np.stack(table, axis=1)
+        self.table = np.stack(table, axis=1).astype(np.uint8)
 
         # sort by efficacy
         # TODO: bitmaps!
-        l = [(np.sum(table[i, :]), i) for i in range(self.num_tests)]
-        l.sort(key=lambda x: x[0], reverse=True)
-        self.sorted_tests = [x[1] for x in l]
+        # 1. calculate bitmaps for each test
+        # 2. (in solve?) maintain current mask of differentiated diseases
+        # 3. 
+        # l = [(np.sum(table[i, :]), i) for i in range(self.num_tests)]
+        # l.sort(key=lambda x: x[0], reverse=True)
+        # self.sorted_tests = [x[1] for x in l]
 
     def data_parse(self, filename: str):
         try:
@@ -131,22 +136,26 @@ class BFSIPSolver:
             exit(1)
 
     def solve(self) -> float:
-        branch_var = self.get_branch_var_sequential([-1] * self.num_tests)
-        self.search(branch_var, [-1] * self.num_tests)
+        branch_var = self.get_branch_var_goated([-1] * self.num_tests, np.zeros(self.table.shape[1], dtype=np.uint8))
+        self.search(branch_var, [-1] * self.num_tests, np.zeros(self.table.shape[1], dtype=np.uint8))
+        self.search(-branch_var, [-1] * self.num_tests, np.zeros(self.table.shape[1], dtype=np.uint8))
 
         while self.heap:
-            _, assignments = heapq.heappop(self.heap)
-            branch_var = self.get_branch_var_sequential(assignments)
+            _, assignments, diff_dis = heapq.heappop(self.heap)
+            branch_var = self.get_branch_var_goated(assignments, diff_dis)
             if branch_var == 0:
                 continue
-            self.search(branch_var, assignments)
-            self.search(-branch_var, assignments[:])
+            self.search(branch_var, assignments[:], diff_dis.copy())
+            self.search(-branch_var, assignments, diff_dis)
             
         return self.incumbent_cost
 
-    def search(self, branch_var: int, fixed: list[int]):
+    def search(self, branch_var: int, fixed: list[int], diff_dis: np.ndarray):
         fixed[abs(branch_var) - 1] = 1 if branch_var > 0 else 0
         feasible, objective_value, lp_assignments = self.lp_solver.solve(fixed)
+
+        if branch_var > 0:
+            diff_dis = diff_dis | self.table[abs(branch_var) - 1]
         
         if not feasible:
             pass
@@ -157,14 +166,14 @@ class BFSIPSolver:
                 self.incumbent_cost = objective_value
                 self.incumbent_assignment = lp_assignments
         else:
-            heapq.heappush(self.heap, (objective_value, fixed))
+            heapq.heappush(self.heap, (objective_value, fixed, diff_dis))
         
 
-    def get_branch_var_sorted(self, assignments: list[int]) -> int:
-        for i in self.sorted_tests:
-            if assignments[i] == -1:
-                return i + 1
-        return 0
+    # def get_branch_var_sorted(self, assignments: list[int]) -> int:
+    #     for i in self.sorted_tests:
+    #         if assignments[i] == -1:
+    #             return i + 1
+    #     return 0
     
 
     def get_branch_var_sequential(self, assignments: list[int]) -> int:
@@ -172,3 +181,30 @@ class BFSIPSolver:
             if val == -1:
                 return i + 1
         return 0
+
+    def get_branch_var_goated(self, assignments: list[int], diff_dis: np.ndarray) -> int:
+        # [1, 0, -1]
+        print("\n\ngetting a goated new branch var")
+        print(f"current assignments -- {assignments}")
+        print(f"current diff_dis -- {diff_dis}")
+        unassigned = [i for i, val in enumerate(assignments) if val == -1]
+        print(f"unassigned -- {unassigned}")
+        print(f"table -- {self.table}")
+        if not unassigned: return 0
+
+        nondiff_dis = 1 - diff_dis
+
+        unassigned_rows = self.table[unassigned]
+        new_diffs = np.bitwise_and(unassigned_rows, nondiff_dis)
+        print(f"new_diffs -- {new_diffs}")
+        new_diff_counts = np.sum(new_diffs, axis=1)
+        # TODO: divide by the cost
+
+        print(f"counts -- {new_diff_counts}")
+
+        best_i = np.argmax(new_diff_counts)
+        best_var = unassigned[best_i]
+
+        print(f"RETURNING -- {best_var + 1}")
+
+        return best_var + 1
--- src/lp/python/main.py
+++ src/lp/python/main.py
@@ -76,4 +76,5 @@ def test():
         
 
 if __name__ == "__main__":
-	test()
+	# test()
+    ipsolve()
